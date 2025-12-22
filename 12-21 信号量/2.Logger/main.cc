c839680831d394a86ed4ca29a276d84257542682
coder2970 <13955275327@163.com>
2025-12-22 15:46:06 +0800

    RAII风格的日志构建与输出刷新

---
+++ 12-21 信号量/2.Logger/main.cc
@@ -0,0 +1,26 @@
+#include "Logger.hpp"
+int main()
+{
+    EnableFileLogStragegy();
+    LOG(LogLevel::ERROR) << "hello world" << "1234" << 3.14;
+    LOG(LogLevel::WARING) << "hello world" << "1234" << 3.14;
+    LOG(LogLevel::FATAL) << "hello world" << "1234" << 3.14;
+    LOG(LogLevel::INFO) << "hello world" << "1234" << 3.14;
+    return 0;
+}
+// int main()
+// {
+//     // std::unique_ptr<LogStragegy> loggerptr = std::make_unique<ConsoleLogStragegy>();
+//     // loggerptr->SyncLog(GetCurrentTime());
+//     // sleep(1);
+//     // loggerptr->SyncLog(GetCurrentTime());
+//     // sleep(1);
+//     // loggerptr->SyncLog(GetCurrentTime());
+//     // sleep(1);
+//     // loggerptr->SyncLog(GetCurrentTime());
+//     // sleep(1);
+//     // loggerptr->SyncLog(GetCurrentTime());
+//     // sleep(1);
+
+//     return 0;
+// }
